f250c167377281322ae42244a426844592c4013d
Craig Tait <taitcraigd@gmail.com>
2023-08-01 07:51:25 +0100

    Additonal examples

--- decorators.py
+++ decorators.py
@@ -55,4 +55,76 @@ try:
     print("Result:", result)
 except MyCustomException as e:
     print("Caught exception:", e)
+class MyCustomException(Exception):
+    pass
+
+def ensure_keys_in_dict(keys: list):
+    def decorator(cls):
+        original_init = cls.__init__
+
+        def new_init(self, *args, **kwargs):
+            environment = args[0] if args else kwargs.get("environment", None)
+            if environment is None:
+                raise MyCustomException("Environment object not provided")
+
+            for key in keys:
+                if key not in kwargs and key not in environment:
+                    raise MyCustomException(f"{key} is required")
+
+            original_init(self, *args, **kwargs)
+
+        cls.__init__ = new_init
+        return cls
+
+    return decorator
+
+# Usage of the decorator
+@ensure_keys_in_dict(["VAR1", "VAR2", "HOME"])  # List of keys to check
+class MyClass:
+    def __init__(self, environment):
+        self.var1 = environment.get("VAR1")
+        self.var2 = environment.get("VAR2")
+
+# Example usage
+import os
+
+try:
+    obj = MyClass(environment=os.environ)
+    print("Object created successfully.")
+except MyCustomException as e:
+    print("Caught exception:", e)
+class MyCustomException(Exception):
+    pass
+
+def ensure_keys_in_dict(keys: list):
+    def decorator(cls):
+        original_init = cls.__init__
+
+        def new_init(self, *args, **kwargs):
+            for key in keys:
+                if key not in kwargs and key not in args[0]:
+                    raise MyCustomException(f"{key} is required")
+            original_init(self, *args, **kwargs)
+
+        cls.__init__ = new_init
+        return cls
+
+    return decorator
+
+# Usage of the decorator
+@ensure_keys_in_dict(["VAR1", "VAR2", "HOME"])  # List of keys to check
+class MyClass:
+    def __init__(self, environment):
+        self.var1 = environment.get("VAR1")
+        self.var2 = environment.get("VAR2")
+
+# Example usage
+import os
+
+try:
+    obj = MyClass(os.environ)
+    print("Object created successfully.")
+except MyCustomException as e:
+    print("Caught exception:", e)
+
 
